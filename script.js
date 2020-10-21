27a17d8510c242d3e49e6bc8e25d84fd4a2655ee
Test <71158104+JavierGl-debug@users.noreply.github.com>
2020-10-21 09:56:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var cardsArray = [{
+	'name': 'Shell',
+	'img': 'img/blueshell.png'
+}, {
+	"name": 'Star',
+	'img': 'img/star.jpeg'
+}, {
+	"name": 'Bobomb',
+	'img': 'img/bobomb.png'
+},{
+	"name": 'Bulletbill',
+	'img': 'img/bullet.png'
+},{
+	"name": 'Luigi',
+	'img': 'img/lui.png'
+},{
+	"name": 'Coin',
+	'img': 'img/coin.png'
+},{
+	"name": 'Peach',
+	'img': 'img/princes.jpg'
+},{
+	"name": 'Goomba',
+	'img': 'img/goomba.png'
+},{
+	"name": 'Mushroom',
+	'img': 'img/mushroom.png'
+},{
+	"name": 'Thwomp',
+	'img': 'img/thwomp.png'
+},{
+	"name": '1up',
+	'img': 'img/oneup.png'
+},{
+	"name": 'Mario',
+	'img': 'img/papers.png'
+},
+]
